abf047897f46863b1c26196928542e38e1dc3310
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-22 15:45:23 +0300

    Add files via upload

---
+++ For.java
@@ -0,0 +1,14 @@
+package test;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		for (a=0 ; a<50 ; a++) {
+			System.out.println("Emre Üçbudak");
+		}
+
+	}
+
+}
